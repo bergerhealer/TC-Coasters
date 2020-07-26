25547ea0c1faf137a6b2724f530a7178be2f6b96
Irmo van den Berge <irmo.vandenberge@ziggo.nl>
2020-07-26 21:27:16 +0200

    Make track object place/break sounds less loud
    My ears :(

--- src/main/java/com/bergerkiller/bukkit/coasters/editor/object/ObjectEditState.java
+++ src/main/java/com/bergerkiller/bukkit/coasters/editor/object/ObjectEditState.java
@@ -266,7 +266,7 @@ public class ObjectEditState {
                     editObject.connection.addObject(editObject.object.clone());
 
                     // Play a sound cue so the player knows an object was placed
-                    PlayerUtil.playSound(getPlayer(), SoundEffect.CLICK_WOOD, 0.5f, 1.0f);
+                    PlayerUtil.playSound(getPlayer(), SoundEffect.CLICK_WOOD, 0.1f, 1.0f);
                 } else {
                     // Finish the drag action and switch to duplicate mode
                     try {
@@ -1076,7 +1076,7 @@ public class ObjectEditState {
 
         // Play a sound cue so the player knows objects were added
         if (objectsWereAdded) {
-            PlayerUtil.playSound(getPlayer(), SoundEffect.CLICK_WOOD, 0.5f, 1.0f);
+            PlayerUtil.playSound(getPlayer(), SoundEffect.CLICK_WOOD, 0.1f, 1.0f);
         }
 
         // Remove excess objects
@@ -1094,7 +1094,7 @@ public class ObjectEditState {
             this.duplicatedObjects.clear();
 
             // Play a sound cue so the player knows objects were removed
-            PlayerUtil.playSound(getPlayer(), SoundEffect.ITEM_BREAK, 0.5f, 1.0f);
+            PlayerUtil.playSound(getPlayer(), SoundEffect.ITEM_BREAK, 0.1f, 1.0f);
         }
     }
 
@@ -1106,7 +1106,7 @@ public class ObjectEditState {
             this.duplicatedObjects.subList(startIndex, this.duplicatedObjects.size()).clear();
 
             // Play a sound cue so the player knows objects were removed
-            PlayerUtil.playSound(getPlayer(), SoundEffect.ITEM_BREAK, 0.5f, 1.0f);
+            PlayerUtil.playSound(getPlayer(), SoundEffect.ITEM_BREAK, 0.1f, 1.0f);
         }
     }
 
