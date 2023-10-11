8c182399aead27813f6ddb881e25761a694231d9
Irmo van den Berge <irmo.vandenberge@ziggo.nl>
2023-10-11 19:12:32 +0200

    Even Satan fears Bukkit sometimes
    Signed-off-by: Irmo van den Berge <irmo.vandenberge@ziggo.nl>

--- src/main/java/com/bergerkiller/bukkit/coasters/util/TrackNodePositionReference.java
+++ src/main/java/com/bergerkiller/bukkit/coasters/util/TrackNodePositionReference.java
@@ -9,20 +9,28 @@ import com.bergerkiller.bukkit.coasters.tracks.TrackWorld;
 /**
  * TrackNodeReference that specifies the x/y/z values
  */
-public final class TrackNodePositionReference extends Vector implements TrackNodeReference {
+public final class TrackNodePositionReference implements TrackNodeReference {
+    // Note: MUST use Vector, because otherwise Vector.equals() will break
+    // We already avoid using that function, but other people might still use it
+    // Do NOT make this class extend Vector in some vain attempt to get performance
+    private final Vector position;
 
     public TrackNodePositionReference(double x, double y, double z) {
-        super(x, y, z);
+        this.position = new Vector(x, y, z);
+    }
+
+    public TrackNodePositionReference(Vector position) {
+        this.position = position.clone(); // Fuck Bukkit
     }
 
     @Override
-    public TrackNode findOnWorld(TrackWorld world) {
-        return world.findNodeExact(this);
+    public TrackNode findOnWorld(TrackWorld world, TrackNode excludedNode) {
+        return world.findNodeExact(position, excludedNode);
     }
 
     @Override
     public Vector getPosition() {
-        return this;
+        return position;
     }
 
     @Override
@@ -32,7 +40,7 @@ public final class TrackNodePositionReference extends Vector implements TrackNod
 
     @Override
     public TrackNodeReference reference(TrackWorld world) {
-        TrackNode node = world.findNodeExact(this);
+        TrackNode node = world.findNodeExact(position);
         return (node == null) ? this : node;
     }
 }
